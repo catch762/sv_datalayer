e79a506d69cc201527f8e312c48663c23d5ac399
catch762 <hoh.prichesat@gmail.com>
2026-06-23 21:15:39 +0300

    name test

--- Registrations/Tests/TypeNameTests.cpp
+++ Registrations/Tests/TypeNameTests.cpp
@@ -1,9 +1,28 @@
 #include "sv_common.h"
 #include "doctest/doctest.h"
+#include <cstring>
+#include "DataTypesAndTheirWidgets/DataTypesAndTheirWidgets.h"
+
+template<typename T>
+bool nameIs(const char* name)
+{
+	return std::strcmp(typeName<T>(), name) == 0;
+}
 
 TEST_CASE("Checking that type names are registered")
 {
 	//no need to check actual names - if type is registered, name is correct,
 	//if its not, we get compile error (thats real purpose of this test)
-	CHECK(typeName<bool>() == std::string("bool"));
+
+	CHECK(nameIs<bool>("bool"));
+	CHECK(nameIs<int>("int"));
+	CHECK(nameIs<double>("double"));
+	CHECK(nameIs<QString>("QString"));
+	CHECK(nameIs<LimitedInt>("LimitedInt"));
+	CHECK(nameIs<LimitedIntVec>("LimitedIntVec"));
+	CHECK(nameIs<LimitedDouble>("LimitedDouble"));
+	CHECK(nameIs<LimitedDoubleVec>("LimitedDoubleVec"));
+	CHECK(nameIs<Enum>("Enum"));
+	CHECK(nameIs<EnumVec>("EnumVec"));
+	CHECK(nameIs<BoolVec>("BoolVec"));
 }
