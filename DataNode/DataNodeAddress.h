1d9dc200b837c840bb68c3711f2fdbf6fb6a619d
catch762 <hoh.prichesat@gmail.com>
2026-07-18 20:28:31 +0300

    node address fix

--- DataNode/DataNodeAddress.h
+++ DataNode/DataNodeAddress.h
@@ -1,18 +1,44 @@
 #pragma once
 #include "DataNode.h"
 
-//there will be huge rewrite later, when requirements arrive
+//************************************************************
+//	Assume there is a tree:
+// 
+//		comp("root", {
+//						comp("next", {
+//										leaf("hello", int(5))
+//									 }
+//					 });
+//
+//	And if you pass leaf node to getAbsAddress(), you will get:
+//
+//		"/next/hello"
+//
+//	Because we dont include the root name in address, ever.
+// 
+//  If you try to getAbsAddress() from root, you will get
+//  empty NodeAddressString, which is a valid address with
+//  meaning "take same node".
+// 
+//  I.e. if you try to get child at "/next/hello" from root
+//  node you will get hello node.
+//  I.e. if you try to get child at "" from root
+//  node you will get root node.
+// 
+//  Root is the only node thats allowed to have empty name.
+//************************************************************
 
 using NodeAddressString = QString;
 SV_DECL_ALIASES(NodeAddressString);
 
 inline constexpr auto DataNodeAddressSeparator = "/";
 
-inline NodeAddressStringOrError getAddress(const DataNodeShared& node)
+
+inline NodeAddressStringOrError getAbsAddress(const DataNodeShared& node)
 {
 	if (!node)
 	{
-		return std::format("getAddress error: null node ptr passed");
+		return std::format("getAbsAddress error: null node ptr passed");
 	}
 
 	QString res;
@@ -21,23 +47,24 @@ inline NodeAddressStringOrError getAddress(const DataNodeShared& node)
 
 	while (true)
 	{
-		if (curNode->getName().isEmpty())
-		{
-			return std::format("getAddress error: found node {} with empty name, address up to that: [{}]", curNode, res);
-		}
+		auto parentNode = curNode->tryGetParent();
 
-		res = curNode->getName() + res;
-
-		if (auto parentNode = curNode->tryGetParent())
+		if (!parentNode)
 		{
-			res = DataNodeAddressSeparator + res;
-			curNode = parentNode;
-		}
-		else
-		{
+			//curNode is root. So we are not including it, we stop right there.
 			break;
 		}
+
+		if (curNode->getName().isEmpty())
+		{
+			return std::format("getAbsAddress error: found non-root node {} with empty name, address up to that: [{}]", curNode, res);
+		}
+
+		res = DataNodeAddressSeparator + curNode->getName() + res;
+
+		curNode = parentNode;
 	}
 
+	//may be empty and its fine
 	return res;
 }
