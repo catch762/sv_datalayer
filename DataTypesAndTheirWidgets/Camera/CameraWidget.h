2a0b1f3740a76263cf0d92b46576f985b204d0f0
catch762 <hoh.prichesat@gmail.com>
2026-08-25 20:47:52 +0300

    preparing cam

--- DataTypesAndTheirWidgets/Camera/CameraWidget.h
+++ DataTypesAndTheirWidgets/Camera/CameraWidget.h
@@ -3,8 +3,43 @@
 
 struct CameraData
 {
-	std::array<float, 4> pos	= {};
-	std::array<float, 4> lookat = {};
+	glm::vec3 pos		= {};
+	float	  roll		= 0;
+	glm::vec3 lookat	= glm::vec3(0, -1, 0);
+	float	  fov		= 45;
 };
 
 SV_REGTYPENAME(CameraData);
+
+class CameraViewport : public QWidget
+{
+public:
+	CameraViewport(QWidget* parent = nullptr) : QWidget(parent)
+	{
+		setFixedSize(300, 300);
+	}
+
+    void keyPressEvent(QKeyEvent* event) override
+    {
+        QWidget::keyPressEvent(event);
+    }
+
+    void mousePressEvent(QMouseEvent* event) override
+    {
+        //if (event->button() == Qt::LeftButton)
+
+        QWidget::mousePressEvent(event);
+    }
+    void mouseMoveEvent(QMouseEvent* event) override
+    {
+        QWidget::mouseMoveEvent(event);
+    }
+
+    void mouseReleaseEvent(QMouseEvent* event) override
+    {
+        QWidget::mouseMoveEvent(event);
+    }
+
+private:
+    CameraData camera;
+};
