947f622ec3c78a0dc61678d7667635fb3458a660
catch762 <hoh.prichesat@gmail.com>
2026-09-01 00:23:09 +0300

    okay a lot more done; added untested "also return clipped world point"; next draw line with two gradient lines

--- DataTypesAndTheirWidgets/Camera/CameraWidget.h
+++ DataTypesAndTheirWidgets/Camera/CameraWidget.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "sv_qtcommon.h"
 #include <QTimer>
+#include <QTextLayout>
 
 struct CameraData
 {
@@ -18,15 +19,19 @@ class CameraViewport : public QWidget
 public:
 	CameraViewport(QWidget* parent = nullptr) : QWidget(parent)
 	{
-		setFixedSize(300, 300);
+        setFocusPolicy(Qt::ClickFocus);
 
-        camera.setPos({ 0, 3, 0 });
+		setMinimumSize(50, 50);
+
+        camera.setPos({ 3, 3, 3 });
         camera.lookAtWithoutRoll({ 0, 0, 0 });
 
         updateTimer.setSingleShot(false);
         updateTimer.setInterval(0);
         connect(&updateTimer, &QTimer::timeout, this, &CameraViewport::onUpdate);
 	}
+
+    using RenderFunc = std::function<void(CameraViewport& vp, QPainter& p)>;
 
     int getKey(QKeyEvent* event)
     {
@@ -36,6 +41,17 @@ public:
 
     void keyPressEvent(QKeyEvent* event) override
     {
+        static const QSet<int> keysToTrack = {  Qt::Key_W,
+                                                Qt::Key_A,
+                                                Qt::Key_S,
+                                                Qt::Key_D,
+                                                Qt::Key_Up,
+                                                Qt::Key_Down,
+                                                Qt::Key_Left,
+                                                Qt::Key_Right,
+                                                Qt::Key_Space, 
+                                                Qt::Key_Control };
+
         auto theKey = getKey(event);
 
         if (keysToTrack.contains(theKey))
@@ -98,21 +114,153 @@ public:
     {
         QPainter p(this);
 
-        p.fillRect(rect(), QColor(200, 200, 200));
-
-        auto setColor = [&](QColor color)
+        if (renderFunc)
         {
-            QPen linePen = QPen(color, 3);
-            p.setPen(linePen);
+            renderFunc(*this, p);
+        }
+        else
+        {
+            //default render: just draw world coord system
+            fillWithColor(p);
+            drawWorldAxes(p);
+        }
+    }
+
+    BasicPlaneCamera& getCamera()
+    {
+        return camera;
+    }
+    const BasicPlaneCamera& getCamera() const
+    {
+        return camera;
+    }
+
+    void setRenderFunc(const RenderFunc& func)
+    {
+        renderFunc = func;
+    }
+
+    //Painting methods:
+public:
+    void drawWorldAxes(QPainter& p)
+    {
+        drawAxes(p, { 0,0,0 }, { 1,0,0 }, { 0,1,0 }, { 0,0,1 });
+    }
+
+    //this draws camera forward dir as Z axis, although its -Z really
+    void drawCameraAxes(QPainter& p, const BasicPlaneCamera& camera)
+    {
+        drawAxes(p, camera.getPos(), camera.getDirRight(), camera.getDirUp(), camera.getDir());
+    }
+
+    void drawGrid(QPainter& p, glm::vec3 center, float lineSpacing, int linesCount, QPen xPen, QPen yPen, QPen zPen)
+    {
+        const float lineLength = lineSpacing * (linesCount) * 2.0;
+
+        auto drawLinesAtLevel = [&](glm::vec3 midPos, glm::vec3 normalizedDir, glm::vec3 differenceDir)
+        {
+            for (int i = -linesCount; i <= linesCount; ++i)
+            {
+                glm::vec3 thisMidpos    = midPos + differenceDir * lineSpacing * float(i);
+                glm::vec3 thisLineBegin = thisMidpos - normalizedDir * lineLength * 0.5f;
+                glm::vec3 thisLineEnd   = thisMidpos + normalizedDir * lineLength * 0.5f;
+
+                drawWorldLine(p, thisLineBegin, thisLineEnd);
+            }
         };
 
-        setColor(Qt::red);
-        drawWorldLine(p, {}, { 1,0,0 });
-        setColor(Qt::green);
-        drawWorldLine(p, {}, { 0,1,0 });
-        setColor(Qt::blue);
-        drawWorldLine(p, {}, { 0,0,1 });
+        auto drawLinesAlongOtherAxis = [&]( glm::vec3 masterMidPos,
+                                            glm::vec3 masterDifferenceDir,
+                                            glm::vec3 subNormalizedDir,     glm::vec3 subDifferenceDir)
+        {
+            for (int i = -linesCount; i <= linesCount; ++i)
+            {
+                glm::vec3 thisMidpos = masterMidPos + masterDifferenceDir * lineSpacing * float(i);
+
+                drawLinesAtLevel(thisMidpos, subNormalizedDir, subDifferenceDir);
+            }
+        };
+
+        const glm::vec3 XAxis(1, 0, 0);
+        const glm::vec3 YAxis(0, 1, 0);
+        const glm::vec3 ZAxis(0, 0, 1);
+
+      
+        //Layers on Y, containing lines in X direction, that are spaced by Z
+        p.setPen(xPen);
+        drawLinesAlongOtherAxis(center, YAxis, XAxis, ZAxis);
+
+        p.setPen(zPen);
+        drawLinesAlongOtherAxis(center, YAxis, ZAxis, XAxis);
+
+        p.setPen(yPen);
+        drawLinesAlongOtherAxis(center, XAxis, YAxis, ZAxis);
     }
+
+    void drawStandardGrid(QPainter& p)
+    {
+        drawGrid(p, {}, 1, 1, QPen(Qt::cyan), QPen(Qt::magenta), QPen(Qt::yellow));
+    }
+
+    void drawAxes(QPainter& p, glm::vec3 pos, glm::vec3 dirX, glm::vec3 dirY, glm::vec3 dirZ, qreal lineWidth = 3)
+    {
+        setPen(p, Qt::red, lineWidth);
+        drawWorldLine(p, pos, pos + dirX);
+        setPen(p, Qt::green, lineWidth);
+        drawWorldLine(p, pos, pos + dirY);
+        setPen(p, Qt::blue, lineWidth);
+        drawWorldLine(p, pos, pos + dirZ);
+    }
+
+    void fillWithColor(QPainter& p, QColor color = QColor(200, 200, 200))
+    {
+        p.fillRect(rect(), color);
+    }
+
+    void setPen(QPainter& p, QColor color, qreal width = 3)
+    {
+        QPen linePen = QPen(color, width);
+        p.setPen(linePen);
+    }
+
+    //uses current pen
+    void drawWorldLine(QPainter& painter, glm::vec3 worldBegin, glm::vec3 worldEnd)
+    {
+        auto clippedLine = worldLineToScreen(camera.getViewProjection(), worldBegin, worldEnd);
+        if (!clippedLine) return;
+
+        auto beginNdc    = clippedLine->first;
+        auto endNdc      = clippedLine->second;
+
+        QPointF beginPix = pixCoordOfNdcCoord(rect(), beginNdc);
+        QPointF endPix   = pixCoordOfNdcCoord(rect(), endNdc);
+
+        painter.drawLine(beginPix, endPix);
+    }
+
+    // Tries to print text within rectangle that is like rect() but starts at startY;
+    // Prints text on multiple lines if needed.
+    // returns 'endY' - coordinates of last line actually printed.
+    int printText(QPainter& p, const QString& text, int startY = 0)
+    {
+        static const QFont fixedFont("Arial", 12);
+        p.setFont(fixedFont);
+
+        QRect requestedRect = rect();
+        requestedRect.setTop(startY);
+
+        // To ensure we use same flags for metrics and painter:
+        int flags = Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap;
+
+        QRect actualTextBoundingRect = p.boundingRect(requestedRect, flags, text);
+
+        p.drawText(requestedRect, flags, text);
+
+        return actualTextBoundingRect.bottom();
+    }
+
+signals:
+    void cameraChanged(const BasicPlaneCamera& camera);
 
 private:
     void setUpdatesEnabled(bool enabled)
@@ -124,12 +272,19 @@ private:
     void applyMousePitchYawChange(QPoint mouseDelta)
     {
         glm::vec3 pitchYawRoll = glm::vec3(-mouseDelta.y(), -mouseDelta.x(), 0);
+        //glm::vec3 pitchYawRoll = glm::vec3(-mouseDelta.y(), 0, mouseDelta.x());
+
         pitchYawRoll *= 0.008;
 
-        camera.addAngles(pitchYawRoll);
+        if (glm::length(pitchYawRoll) > 0.000001)
+        {
+            camera.addAngles(pitchYawRoll);
+            emit cameraChanged(camera);
+        }
     }
 
-    void applyMovementTick()
+    //returns true if camera changed
+    bool applyMovementByKeys()
     {
         float moveForward = 0;
         float moveUpward = 0;
@@ -161,36 +316,62 @@ private:
         }
 
         auto moveVec = glm::vec3{ moveRight, moveUpward, moveForward } * moveSpeed;
-        camera.moveBy(moveVec);
+
+        if (glm::length(moveVec) > 0.000001)
+        {
+            camera.moveBy(moveVec);
+            return true;
+        }
+        else return false;
+    }
+
+    //returns true if camera changed
+    bool applyRotationByKeys()
+    {
+        float deltaPitch = 0;
+        float deltaYaw = 0;
+        float deltaRoll = 0;
+
+        if (keysPressed.contains(Qt::Key_Up))
+        {
+            deltaPitch += 1;
+        }
+        if (keysPressed.contains(Qt::Key_Down))
+        {
+            deltaPitch -= 1;
+        }
+        if (keysPressed.contains(Qt::Key_Left))
+        {
+            deltaYaw += 1;
+        }
+        if (keysPressed.contains(Qt::Key_Right))
+        {
+            deltaYaw -= 1;
+        }
+
+        glm::vec3 pitchYawRollDeltasRadians = { deltaPitch, deltaYaw, deltaRoll };
+
+        pitchYawRollDeltasRadians *= 0.003;
+
+        if (glm::length(pitchYawRollDeltasRadians) > 0.00001)
+        {
+            camera.addAngles(pitchYawRollDeltasRadians);
+            return true;
+        }
+        else return false;
     }
 
     void onUpdate()
     {
-        applyMovementTick();
-        update();
-    }
+        bool changed1 = applyMovementByKeys();
+        bool changed2 = applyRotationByKeys();
 
-    void drawWorldLine(QPainter& painter, glm::vec3 worldBegin, glm::vec3 worldEnd)
-    {
-        auto beginNdc = camera.worldToScreen11(worldBegin);
-        //if (!beginNdc) return;
-
-        auto endNdc = camera.worldToScreen11(worldEnd);
-        //if (!endNdc) return;
-
+        if (changed1 || changed2)
         {
-            auto clippedLine = worldLineToScreen(camera.getViewProjection(), worldBegin, worldEnd);
-            if (!clippedLine) return;
-
-            beginNdc = clippedLine->first;
-            endNdc = clippedLine->second;
+            emit cameraChanged(camera);
         }
 
-
-        QPointF beginPix = pixCoordOfNdcCoord(rect(), *beginNdc);
-        QPointF endPix   = pixCoordOfNdcCoord(rect(), *endNdc);
-
-        painter.drawLine(beginPix, endPix);
+        update();
     }
 
 private:
@@ -204,5 +385,71 @@ private:
 
     QPoint lastMousePos;
 
-    inline static const QSet<int> keysToTrack = { Qt::Key_W, Qt::Key_A, Qt::Key_S, Qt::Key_D, Qt::Key_Space, Qt::Key_Control };
+    RenderFunc renderFunc;
+};
+
+// 1 or 0.
+// This adds second viewport, which renders same as first one, but with external camera,
+// and you also can see first camera.
+#define CAMERAWIDGET_ENABLE_DEBUGVIEWPORT 1
+
+class CameraWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    CameraWidget(QWidget* parent = nullptr) : QWidget(parent)
+    {
+        layout = new QGridLayout(this);
+
+        //viewport on the left
+        cameraViewport = new CameraViewport(this);
+        cameraViewport->setRenderFunc(std::bind(&CameraWidget::renderScene, std::placeholders::_1, std::placeholders::_2, nullptr));
+        layout->addWidget(cameraViewport, 0, 0);
+
+        //optional debug viewport in the middle
+#if CAMERAWIDGET_ENABLE_DEBUGVIEWPORT
+        cameraViewportDbg = new CameraViewport(this);
+        cameraViewportDbg->setRenderFunc(std::bind(&CameraWidget::renderScene, std::placeholders::_1, std::placeholders::_2, cameraViewport));
+
+        connect(cameraViewport, &CameraViewport::cameraChanged, this,
+            [this]()
+            {
+                cameraViewportDbg->update();
+            });
+
+        layout->addWidget(cameraViewportDbg, 0, 1);
+#endif
+
+        int controlsLayoutColumn = 1 + int(CAMERAWIDGET_ENABLE_DEBUGVIEWPORT);
+
+        //all controls on the right
+        controlsLayout = new QGridLayout();
+        layout->addLayout(controlsLayout, 0, controlsLayoutColumn);
+    }
+
+private:
+    static void renderScene(CameraViewport& vp, QPainter& p, const CameraViewport* additionalCameraToRender = nullptr)
+    {
+        vp.fillWithColor(p, additionalCameraToRender ? QColor(0, 0, 0) : QColor(55, 55, 55));
+
+        vp.drawStandardGrid(p);
+
+        vp.drawWorldAxes(p);
+
+        if (additionalCameraToRender)
+        {
+            vp.drawCameraAxes(p, additionalCameraToRender->getCamera());
+        }
+
+        int textY = 0;
+        textY = vp.printText(p, QString::fromStdString(vp.getCamera().toString()), textY);
+    }
+
+private:
+    QGridLayout*    layout                  = nullptr;
+    CameraViewport*     cameraViewport      = nullptr;
+#if CAMERAWIDGET_ENABLE_DEBUGVIEWPORT
+    CameraViewport*     cameraViewportDbg  = nullptr;
+#endif
+    QGridLayout*        controlsLayout      = nullptr;
 };
