b9e4426ec191da0b1b1727c09cfca58e0bd7584c
catch762 <hoh.prichesat@gmail.com>
2026-09-01 17:35:13 +0300

    move some to cpp

--- DataTypesAndTheirWidgets/Camera/CameraWidget.cpp
+++ DataTypesAndTheirWidgets/Camera/CameraWidget.cpp
@@ -1,2 +1,55 @@
 #include "CameraWidget.h"
 
+CameraWidget::CameraWidget(QWidget* parent) : QWidget(parent)
+{
+    layout = new QGridLayout(this);
+
+    //viewport on the left
+    cameraViewport = new CameraViewport(this);
+    cameraViewport->setRenderFunc(std::bind(&CameraWidget::renderScene, std::placeholders::_1, std::placeholders::_2, nullptr));
+    layout->addWidget(cameraViewport, 0, 0);
+
+    //optional debug viewport in the middle
+#if CAMERAWIDGET_ENABLE_DEBUGVIEWPORT
+    cameraViewportDbg = new CameraViewport(this);
+    cameraViewportDbg->setRenderFunc(std::bind(&CameraWidget::renderScene, std::placeholders::_1, std::placeholders::_2, cameraViewport));
+
+    connect(cameraViewport, &CameraViewport::cameraChanged, this,
+        [this]()
+        {
+            cameraViewportDbg->update();
+        });
+
+    layout->addWidget(cameraViewportDbg, 0, 1);
+#endif
+
+    int controlsLayoutColumn = 1 + int(CAMERAWIDGET_ENABLE_DEBUGVIEWPORT);
+
+    //layout for all controls on the right
+    controlsLayout = new QGridLayout();
+    layout->addLayout(controlsLayout, 0, controlsLayoutColumn);
+
+    //the controls:
+    {
+
+    }
+
+    setMinimumSize(400, 200);
+}
+
+void CameraWidget::renderScene(CameraViewport& vp, QPainter& p, const CameraViewport* additionalCameraToRender)
+{
+    vp.fillWithColor(p, additionalCameraToRender ? QColor(0, 0, 0) : QColor(55, 55, 55));
+
+    vp.drawStandardGrid(p);
+
+    vp.drawWorldAxes(p);
+
+    if (additionalCameraToRender)
+    {
+        vp.drawCameraAxes(p, additionalCameraToRender->getCamera());
+    }
+
+    int textY = 0;
+    textY = vp.printText(p, QString::fromStdString(vp.getCamera().toString()), Qt::white, textY);
+}
--- DataTypesAndTheirWidgets/Camera/CameraWidget.h
+++ DataTypesAndTheirWidgets/Camera/CameraWidget.h
@@ -494,68 +494,18 @@ private:
 };
 
 // 1 or 0.
-// This adds second viewport, which renders same as first one, but with external camera,
-// and you also can see first camera.
+// This adds second viewport, which renders same as first one, but with second camera,
+// and you also can see first camera in that view. Helps debugging camera rotation code.
 #define CAMERAWIDGET_ENABLE_DEBUGVIEWPORT 1
 
 class CameraWidget : public QWidget
 {
     Q_OBJECT
 public:
-    CameraWidget(QWidget* parent = nullptr) : QWidget(parent)
-    {
-        layout = new QGridLayout(this);
-
-        //viewport on the left
-        cameraViewport = new CameraViewport(this);
-        cameraViewport->setRenderFunc(std::bind(&CameraWidget::renderScene, std::placeholders::_1, std::placeholders::_2, nullptr));
-        layout->addWidget(cameraViewport, 0, 0);
-
-        //optional debug viewport in the middle
-#if CAMERAWIDGET_ENABLE_DEBUGVIEWPORT
-        cameraViewportDbg = new CameraViewport(this);
-        cameraViewportDbg->setRenderFunc(std::bind(&CameraWidget::renderScene, std::placeholders::_1, std::placeholders::_2, cameraViewport));
-
-        connect(cameraViewport, &CameraViewport::cameraChanged, this,
-            [this]()
-            {
-                cameraViewportDbg->update();
-            });
-
-        layout->addWidget(cameraViewportDbg, 0, 1);
-#endif
-
-        int controlsLayoutColumn = 1 + int(CAMERAWIDGET_ENABLE_DEBUGVIEWPORT);
-
-        //layout for all controls on the right
-        controlsLayout = new QGridLayout();
-        layout->addLayout(controlsLayout, 0, controlsLayoutColumn);
-
-        //the controls:
-        {
-            
-        }
-
-        setMinimumSize(400, 200);
-    }
+    CameraWidget(QWidget* parent = nullptr);
 
 private:
-    static void renderScene(CameraViewport& vp, QPainter& p, const CameraViewport* additionalCameraToRender = nullptr)
-    {
-        vp.fillWithColor(p, additionalCameraToRender ? QColor(0, 0, 0) : QColor(55, 55, 55));
-
-        vp.drawStandardGrid(p);
-
-        vp.drawWorldAxes(p);
-
-        if (additionalCameraToRender)
-        {
-            vp.drawCameraAxes(p, additionalCameraToRender->getCamera());
-        }
-
-        int textY = 0;
-        textY = vp.printText(p, QString::fromStdString(vp.getCamera().toString()), Qt::white, textY);
-    }
+    static void renderScene(CameraViewport& vp, QPainter& p, const CameraViewport* additionalCameraToRender = nullptr);
 
 private:
     QGridLayout*    layout                  = nullptr;
